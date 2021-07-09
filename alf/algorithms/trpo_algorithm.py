162e54b872b62788f6a05fd86db290b72927d2d7
runjerry <qinxun.bai@gmail.com>
2021-07-08 22:15:21 -0700

    TRPO algorithm on bullet_humanoid

---
+++ alf/algorithms/trpo_algorithm.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""TRPO algorithm."""
+
+from collections import namedtuple
+import gin
+import torch
+
+import alf
+from alf.algorithms.actor_critic_algorithm import ActorCriticAlgorithm
+from alf.algorithms.actor_critic_algorithm import ActorCriticState, ActorCriticInfo
+from alf.algorithms.trpo_loss import TRPOLoss
+from alf.data_structures import TimeStep, AlgStep, namedtuple, Experience
+from alf.networks import ActorDistributionNetwork, ValueNetwork
+from alf.tensor_specs import TensorSpec
+from alf.utils import common, value_ops, dist_utils
+from .config import TrainerConfig
+
+TRPOInfo = namedtuple(
+    "TRPOInfo", [
+        "action_distribution", "value", "returns", "advantages",
+        "prev_actor_state"
+    ],
+    default_value=())
+
+# TRPOInfo = namedtuple("TRPOInfo",
+#     ["action_distribution", "returns", "advantages"])
+
+
+@alf.configurable
+class TRPOAlgorithm(ActorCriticAlgorithm):
+    """TRPO Algorithm.
+    Implement the simplified surrogate loss in equation (9) of "Trust Region
+    Policy Optimization" https://arxiv.org/abs/1502.05477
+
+    It works with ``trpo_loss.TRPOLoss``. 
+    """
+
+    def is_on_policy(self):
+        return False
+
+    def preprocess_experience(self, exp: Experience):
+        """Compute advantages and put it into exp.rollout_info."""
+        advantages = value_ops.generalized_advantage_estimation(
+            rewards=exp.reward,
+            values=exp.rollout_info.value,
+            step_types=exp.step_type,
+            discounts=exp.discount * self._loss._gamma,
+            td_lambda=self._loss._lambda,
+            time_major=False)
+        advantages = torch.cat([
+            advantages,
+            torch.zeros(*advantages.shape[:-1], 1, dtype=advantages.dtype)
+        ],
+                               dim=-1)
+        returns = exp.rollout_info.value + advantages
+        return exp._replace(
+            rollout_info=TRPOInfo(
+                action_distribution=exp.rollout_info.action_distribution,
+                returns=returns,
+                advantages=advantages))
+
+    # def predict_step(self, time_step: TimeStep, state: ActorCriticState,
+    #                  epsilon_greedy):
+    #     """Predict for one step."""
+    #     action_dist, actor_state = self._actor_network(
+    #         time_step.observation, state=state.actor)
+
+    #     action = dist_utils.epsilon_greedy_sample(action_dist, epsilon_greedy)
+    #     return AlgStep(
+    #         output=action,
+    #         state=ActorCriticState(actor=actor_state),
+    #         info=TRPOInfo(action_distribution=action_dist))
+
+    def rollout_step(self, time_step: TimeStep, state: ActorCriticState):
+        """Rollout for one step."""
+        value, value_state = self._value_network(
+            time_step.observation, state=state.value)
+
+        action_distribution, actor_state = self._actor_network(
+            time_step.observation, state=state.actor)
+
+        action = dist_utils.sample_action_distribution(action_distribution)
+        return AlgStep(
+            output=action,
+            state=ActorCriticState(actor=actor_state, value=value_state),
+            info=TRPOInfo(
+                action_distribution=action_distribution,
+                value=value,
+                prev_actor_state=state.actor))
+
+    def calc_loss(self, experience, train_info: TRPOInfo):
+        """Calculate loss."""
+        return self._loss(self._actor_network, experience, train_info)
+
+    def _trainable_attributes_to_ignore(self):
+        return ['_actor_network']
---
+++ alf/examples/trpo_bullet_humanoid_conf.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import partial
+import torch
+
+import alf
+from alf.algorithms.actor_critic_algorithm import ActorCriticAlgorithm
+from alf.environments.suite_gym import wrap_env
+from alf.networks import NormalProjectionNetwork, ActorDistributionNetwork, ValueNetwork
+from alf.optimizers import Adam, AdamTF
+from alf.utils.losses import element_wise_huber_loss
+
+import pybullet_envs
+from alf.nest.utils import NestConcat
+from alf.optimizers import Adam, AdamTF
+from alf.utils.losses import element_wise_squared_loss
+
+import trpo_conf
+
+env_name = "HumanoidBulletEnv-v0"
+
+# environment config
+alf.config(
+    'create_environment', env_name=env_name, num_parallel_environments=96)
+
+alf.config('wrap_env', clip_action=False)
+
+# algorithm config
+fc_layer_params = (32, 32, 32)
+
+actor_network_ctor = partial(
+    ActorDistributionNetwork,
+    fc_layer_params=fc_layer_params,
+    activation=torch.tanh,
+    continuous_projection_net_ctor=partial(
+        NormalProjectionNetwork,
+        projection_output_init_gain=1e-5,
+        std_bias_initializer_value=0.0,
+        std_transform=torch.exp))
+
+value_network_ctor = partial(
+    ValueNetwork, fc_layer_params=fc_layer_params, activation=torch.tanh)
+
+alf.config(
+    'ActorCriticAlgorithm',
+    actor_network_ctor=actor_network_ctor,
+    value_network_ctor=value_network_ctor)
+
+alf.config(
+    'TRPOLoss',
+    entropy_regularization=1e-2,
+    gamma=0.99,
+    td_lambda=0.95,
+    td_error_loss_fn=element_wise_squared_loss,
+    normalize_advantages=True)
+
+# training config
+alf.config(
+    'Agent',
+    optimizer=AdamTF(lr=1e-3, gradient_clipping=0.5, clip_by_global_norm=True))
+
+alf.config(
+    'TrainerConfig',
+    mini_batch_length=1,
+    unroll_length=512,
+    mini_batch_size=4096,
+    num_updates_per_train_iter=20,
+    num_iterations=1000,
+    num_checkpoints=1,
+    evaluate=True,
+    eval_interval=100,
+    num_eval_episodes=5,
+    debug_summaries=False,
+    random_seed=1,
+    summarize_grads_and_vars=False,
+    summary_interval=10)
